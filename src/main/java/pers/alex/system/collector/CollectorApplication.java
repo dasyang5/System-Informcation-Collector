788a75c919ad962e46c0fda997c68fbe18bfab26
Alex Yang <dasyang5@gmail.com>
2019-09-20 16:00:14 +0800

    [ADD] Add spring security,username and password are configured in application.properties.

--- src/main/java/pers/alex/system/collector/CollectorApplication.java
+++ src/main/java/pers/alex/system/collector/CollectorApplication.java
@@ -1,12 +1,15 @@
 package pers.alex.system.collector;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CollectorApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CollectorApplication.class, args);
     }
 
